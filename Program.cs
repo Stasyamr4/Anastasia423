c94c89c2a7839e954eb40998c387a3768e9c9d09
Stasyamr4 <anastasiamura04@mail.ru>
2025-10-07 10:28:40 +0300

    ФУНКЦИЯ САМОГО КОРОТКОГО СЛОВА.

--- Program.cs
+++ Program.cs
@@ -25,18 +25,30 @@ class Program
                 Console.WriteLine("Значение пусто, введите текст");
             }
            
-            int word = CountWords(text);
+            int word = Words(text);
             Console.WriteLine($"количество слов: {word}");
         }
 
-        static int CountWords(string text)
+        static int Words(string text)
         {
             if (string.IsNullOrWhiteSpace(text))
                 return 0;
 
             string[] words = text.Split(new[] { ' ', '\t', '\n', '\r' },
                                       StringSplitOptions.RemoveEmptyEntries);
+
+            string shortestWord = words[0];
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i].Length < shortestWord.Length)
+                {
+                    shortestWord = words[i];
+                }
+            }
+            Console.WriteLine($"Самое короткое слово: '{shortestWord}' (длина: {shortestWord.Length})");
             return words.Length;
+
+
         }
 
 
