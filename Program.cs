f5ab763ab37b6c3cf01cc97956bde22c87feae1e
Stasyamr4 <anastasiamiura04@gmail.com>
2025-10-07 12:04:28 +0300

    Функция для нахождения кол-ва предложений.

--- Program.cs
+++ Program.cs
@@ -3,30 +3,35 @@ using System.Runtime.CompilerServices;
 
 class Program
 {
+    // Список для хранения статистики всех текстов
+    static List<string> text = new List<string>();
     static void Main()
     {
         while (true)
         {
             Console.WriteLine("Введите текст");
             string text = Console.ReadLine();
-            
+
             if (!String.IsNullOrEmpty(text))
             {
-                
+
                 if (text.Length < 100)
                 {
                     Console.WriteLine("\nВведите текст не менее 100 символов\n");
                 }
-                
+
             }
 
             else
             {
                 Console.WriteLine("Значение пусто, введите текст");
             }
-           
+
             int word = Words(text);
             Console.WriteLine($"количество слов: {word}");
+
+            int sentenceCount = CountSentences(text);
+            Console.WriteLine($"Количество предложений: {sentenceCount}");
         }
 
         static int Words(string text)
@@ -47,11 +52,20 @@ class Program
             }
             Console.WriteLine($"Самое короткое слово: '{shortestWord}' (длина: {shortestWord.Length})");
             return words.Length;
-
-
         }
 
+            static int CountSentences(string text)
+            {
+                int count = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+                        count++;
+                }
+                return count;
+            }
 
-    }
-    }
+        }
+    
+}
 
