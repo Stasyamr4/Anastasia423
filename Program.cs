8a7494e7cc412cece715fd78e03b5515f76d9e3f
Stasyamr4 <anastasiamura04@mail.ru>
2025-10-13 11:30:17 +0300

    почти что готовый класс библиотек

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 public enum Genre
 {
@@ -11,26 +12,59 @@ public enum Genre
     Историческая
 }
 
-
-public class Book
+class Program
 {
-    private static int nextId = 1;
-
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public Genre Genre { get; set; }
-    public int Year { get; set; }
-    public decimal Price { get; set; }
-
-    public Book()
+    static List<Books> books = new List<Books>();
+    class Books
     {
-        Id = nextId++;
-    }
+        int id = 0;
+        static int nextID = 1;
+        string title { get; set; } = "";
+        string author { get; set; } = "";
+        string genre { get; set; } = "";
+        int year { get; set; }
+        int price { get; set; }
 
-    public override string ToString() //override - чтобы вызывать было легче
+
+        public Books()
+        {
+            id = nextID++;
+        }
+
+        public override string ToString() //override - чтобы вызывать было легче
+        {
+            return $"ID: {id} | {title} | Автор: {author} | Жанр: {genre} | Год: {year} | Цена: {price} руб.";
+        }
+    }
+        public class Library
+        {
+            public void AddTestData()
+            {
+            var book = new Books();
+                books.AddRange(new[]
+                {
+            new Books { book.title = "1984", author = "Джордж Оруэлл", genre = "Фантастика", year = 1949, price = 500 },
+            new Books { title = "Преступление и наказание", author = "Фёдор Достоевский", genre = "Роман", year = 1866, price = 450 },
+            new Books { title = "Мастер и Маргарита", author = "Михаил Булгаков", genre = "Фантастика", year = 1967, price = 600 },
+            new Books { title = "Шерлок Холмс", author = "Артур Конан Дойл", genre = "Детектив", year = 1887, price = 550 },
+            new Books { title = "Война и мир", author = "Лев Толстой",genre = "Историческая", year = 1869, price = 700 }
+        });
+
+                Console.WriteLine("Добавлено 5 тестовых книг");
+            }
+        }
+
+
+    static void Main()
     {
-        return $"ID: {Id} | {Title} | Автор: {Author} | Жанр: {Genre} | Год: {Year} | Цена: {Price} руб.";
-    }
+        var library = new Library();
+        library.AddTestData();
 
+        // Выводим все книги
+        Console.WriteLine("\n=== ВСЕ КНИГИ В БИБЛИОТЕКЕ ===");
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
+    }
 }
