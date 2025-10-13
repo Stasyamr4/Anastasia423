736d684be4dbe8d0df28b8714bd437df37b37358
Stasyamr4 <anastasiamura04@mail.ru>
2025-10-13 10:52:52 +0300

    создан класс книги

--- Program.cs
+++ Program.cs
@@ -1,100 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
+public enum Genre
 {
-    static void Main()
-    {
-
-    }
+    Фантастика,
+    Детектив,
+    Роман,
+    Научная,
+    Историческая
 }
-enum categories
+
+
+public class Book
 {
-    food = 1,
-    groceries,
-    careProducts
-}
-class Product
-{
-    int id = 0;
-    string name;
-    double price;
-    int countProd = 0;
-    bool isprod;
-    int ProdCateg;
-    public void GetProduct()
+    private static int nextId = 1;
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public Genre Genre { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+
+    public Book()
     {
-        id += 1;
-        Console.WriteLine("Введите назваание товара: ");
-        while (true)
-        {
-            name = Console.ReadLine();
-            if (name == null)
-            {
-                Console.WriteLine("введите название товара еще раз!");
-            }
-            else break;
-        }
-        Console.WriteLine("Введите цену товара: ");
-        while (true)
-        {
-            price = Convert.ToDouble(Console.ReadLine());
-            if ((price == 0) || (price == null))
-            {
-                Console.WriteLine("введите цену товара еще раз!");
-            }
-            else break;
-        }
-        Console.WriteLine("Введите количество товаров: ");
-        while (true)
-        {
-            countProd = Convert.ToInt32(Console.ReadLine());
-            if ((countProd == 0) || (countProd == null))
-            {
-                Console.WriteLine("введите количество товаров еще раз!");
-            }
-            else break;
-        }
-        if (countProd > 0)
-        {
-            isprod = true;
-
-        }
-        else
-        {
-            isprod = false;
-        }
-        ;
-        Console.WriteLine("Введите категорию товара от 1 до 3: ");
-        while (true)
-        {
-            ProdCateg = Convert.ToInt32(Console.ReadLine());
-            if ((ProdCateg == null) || (ProdCateg < 1) || (ProdCateg > 3))
-            {
-                Console.WriteLine("неправвильно введена категория, попробуйте еще раз!");
-            }
-            else break;
-        }
-        categories selectedCategory = (categories)ProdCateg;
-        switch (selectedCategory)
-        {
-            case categories.food:
-                {
-                    Console.WriteLine("Категория -  еда");
-                    break;
-                }
-            case categories.groceries:
-                {
-                    Console.WriteLine("Категория -  бакалея");
-                    break;
-                }
-            case categories.careProducts:
-                {
-                    Console.WriteLine("Категория -  товары для ухода");
-                    break;
-                }
-        }
-
-
-
+        Id = nextId++;
     }
+
+    public override string ToString() //override - чтобы вызывать было легче
+    {
+        return $"ID: {Id} | {Title} | Автор: {Author} | Жанр: {Genre} | Год: {Year} | Цена: {Price} руб.";
+    }
+
 }
