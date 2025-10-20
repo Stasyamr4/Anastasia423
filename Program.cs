39024899aa8a37c285c87f5cd8f69b0ff7d7ad82
Stasyamr4 <anastasiamura04@mail.ru>
2025-10-20 10:52:47 +0300

    класс студента и учителя

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 class Program
 {
     static void Main()
@@ -7,94 +6,67 @@ class Program
 
     }
 }
-enum categories
+class Person
 {
-    food = 1,
-    groceries,
-    careProducts
-}
-class Product
-{
-    int id = 0;
-    string name;
-    double price;
-    int countProd = 0;
-    bool isprod;
-    int ProdCateg;
-    public void GetProduct()
+    private int id;
+    private String FIO;
+    private DateOnly Birthday;
+    private string Gender;
+    public Person(string fio, DateOnly birthday, string gender)
     {
-        id += 1;
-        Console.WriteLine("Введите назваание товара: ");
-        while (true)
-        {
-            name = Console.ReadLine();
-            if (name == null)
-            {
-                Console.WriteLine("введите название товара еще раз!");
-            }
-            else break;
-        }
-        Console.WriteLine("Введите цену товара: ");
-        while (true)
-        {
-            price = Convert.ToDouble(Console.ReadLine());
-            if ((price == 0) || (price == null))
-            {
-                Console.WriteLine("введите цену товара еще раз!");
-            }
-            else break;
-        }
-        Console.WriteLine("Введите количество товаров: ");
-        while (true)
-        {
-            countProd = Convert.ToInt32(Console.ReadLine());
-            if ((countProd == 0) || (countProd == null))
-            {
-                Console.WriteLine("введите количество товаров еще раз!");
-            }
-            else break;
-        }
-        if (countProd > 0)
-        {
-            isprod = true;
-
-        }
-        else
-        {
-            isprod = false;
-        }
-        ;
-        Console.WriteLine("Введите категорию товара от 1 до 3: ");
-        while (true)
-        {
-            ProdCateg = Convert.ToInt32(Console.ReadLine());
-            if ((ProdCateg == null) || (ProdCateg < 1) || (ProdCateg > 3))
-            {
-                Console.WriteLine("неправвильно введена категория, попробуйте еще раз!");
-            }
-            else break;
-        }
-        categories selectedCategory = (categories)ProdCateg;
-        switch (selectedCategory)
-        {
-            case categories.food:
-                {
-                    Console.WriteLine("Категория -  еда");
-                    break;
-                }
-            case categories.groceries:
-                {
-                    Console.WriteLine("Категория -  бакалея");
-                    break;
-                }
-            case categories.careProducts:
-                {
-                    Console.WriteLine("Категория -  товары для ухода");
-                    break;
-                }
-        }
-
-
-
+        FIO = fio;
+        Birthday = birthday;
+        Gender = gender;
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine($"ФИО: {FIO}\nДень рождения: {Birthday}\nПол: {Gender} ");
     }
 }
+class Student : Person
+{
+    private int StudentNumberID;
+    private int CourseNumber;
+    private string Telephone;
+    private string City;
+
+    public Student(string fio, DateOnly birthday, string gender,
+                   int studentNumberId, int courseNumber, string telephone, string city)
+                   : base(fio, birthday, gender)
+    {
+        StudentNumberID = studentNumberId;
+        CourseNumber = courseNumber;
+        Telephone = telephone;
+        City = city;
+    }
+    public override void Print()
+    {
+        Console.WriteLine("Студент");
+        base.Print(); 
+        Console.WriteLine($"Студент ID: {StudentNumberID}\nКурс: {CourseNumber}\nТелефон: {Telephone}\nГород: {City}");
+    }
+}
+class Teacher : Person
+{
+    private string Subject;
+    private string Telephone;
+    private string City;
+
+
+    public Teacher(string fio, DateOnly birthday, string gender, string subject, string telephone, string city)
+        : base(fio, birthday, gender)
+    {
+        Subject = subject;
+        Telephone =telephone;
+        City = city;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Учитель");
+        base.Print();
+        Console.WriteLine($"Предмет: {Subject}\nТелефон: {Telephone}\nГород: {City}");
+    }
+}
+}
+
