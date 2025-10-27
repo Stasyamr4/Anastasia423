e7b2dfc5df3688fb70bf1135abd10dbe8e95bcd4
Stasyamr4 <anastasiamiura04@gmail.com>
2025-10-27 10:25:56 +0300

    создан класс врагов

--- Program.cs
+++ Program.cs
@@ -289,3 +289,44 @@ class Program
             return $"{Name} (Защита: {Defense})";
         }
     }
+    class Enemy
+    {
+        public string Name { get; set; } = "";
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+        public Enemy(string name, int attack, int hp, int defense)
+        {
+            Name = name;
+            HP = hp;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public virtual void AttackPlayer(Player player)
+        {
+            int damage = Attack;
+            player.TakeDamage(damage);
+            Console.WriteLine($"{Name} атакует и наносит {damage} урона!");
+        }
+
+        public void TakeDamage(int damage)
+        {
+            HP -= damage;
+            if (HP < 0) HP = 0;
+            Console.WriteLine($"{Name} получает {damage} урона! Осталось HP: {HP}");
+        }
+
+        public bool IsAlive()
+        {
+            return HP > 0;
+        }
+        public void ShowStats()
+        {
+            Console.WriteLine($"=== {Name} ===");
+            Console.WriteLine($"Здоровье: {HP}");
+            Console.WriteLine($"Атака: {Attack}");
+            Console.WriteLine($"Защита: {Defense}");
+        }
+    }
