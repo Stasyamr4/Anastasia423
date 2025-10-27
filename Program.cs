b4265082d6192457886a32eed91fbb8b72d58c42
Stasyamr4 <anastasiamiura04@gmail.com>
2025-10-27 10:30:09 +0300

    класс наследование гоблин

--- Program.cs
+++ Program.cs
@@ -330,3 +330,25 @@ class Program
             Console.WriteLine($"Защита: {Defense}");
         }
     }
+    class Goblin : Enemy
+    {
+        protected double critChance = 0.2;
+        protected double critMnojitel = 2;
+
+        public Goblin() : base("Гоблин", 8, 30, 3) { }
+
+        public override void AttackPlayer(Player player)
+        {
+            Random random = new Random();
+            double damage = Attack;
+
+            if (random.NextDouble() < critChance)
+            {
+                damage *= critMnojitel;
+                Console.WriteLine("Критический удар!");
+            }
+
+            player.TakeDamage((int)damage);
+            Console.WriteLine($"{Name} атакует и наносит {damage} урона!");
+        }
+    }
