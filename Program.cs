6860128cb70fcdf6e1c6b9e1e166024b655e2265
Stasyamr4 <anastasiamiura04@gmail.com>
2025-10-27 10:05:22 +0300

    создан класс game

--- Program.cs
+++ Program.cs
@@ -1,100 +1,60 @@
 ﻿using System;
+using System.Numerics;
 
 class Program
 {
+
     static void Main()
     {
-
+        Game game = new Game();
+        game.StartGame();
     }
-}
-enum categories
-{
-    food = 1,
-    groceries,
-    careProducts
-}
-class Product
-{
-    int id = 0;
-    string name;
-    double price;
-    int countProd = 0;
-    bool isprod;
-    int ProdCateg;
-    public void GetProduct()
+
+    class Game
     {
-        id += 1;
-        Console.WriteLine("Введите назваание товара: ");
-        while (true)
-        {
-            name = Console.ReadLine();
-            if (name == null)
-            {
-                Console.WriteLine("введите название товара еще раз!");
-            }
-            else break;
-        }
-        Console.WriteLine("Введите цену товара: ");
-        while (true)
-        {
-            price = Convert.ToDouble(Console.ReadLine());
-            if ((price == 0) || (price == null))
-            {
-                Console.WriteLine("введите цену товара еще раз!");
-            }
-            else break;
-        }
-        Console.WriteLine("Введите количество товаров: ");
-        while (true)
-        {
-            countProd = Convert.ToInt32(Console.ReadLine());
-            if ((countProd == 0) || (countProd == null))
-            {
-                Console.WriteLine("введите количество товаров еще раз!");
-            }
-            else break;
-        }
-        if (countProd > 0)
-        {
-            isprod = true;
+        private Random random = new Random();
+        private int turnCount = 0;
 
-        }
-        else
+        public void StartGame()
         {
-            isprod = false;
-        }
-        ;
-        Console.WriteLine("Введите категорию товара от 1 до 3: ");
-        while (true)
-        {
-            ProdCateg = Convert.ToInt32(Console.ReadLine());
-            if ((ProdCateg == null) || (ProdCateg < 1) || (ProdCateg > 3))
+            Player player = new Player(100);
+            Console.WriteLine("=== ТЕКСТОВЫЙ РОГАЛИК ===");
+
+            while (player.HP > 0)
             {
-                Console.WriteLine("неправвильно введена категория, попробуйте еще раз!");
-            }
-            else break;
-        }
-        categories selectedCategory = (categories)ProdCateg;
-        switch (selectedCategory)
-        {
-            case categories.food:
+                turnCount++;
+                Console.WriteLine($"\n--- Ход {turnCount} ---");
+                player.ShowStats();
+
+                // Каждые 10 ходов - босс
+                if (turnCount % 10 == 0)
                 {
-                    Console.WriteLine("Категория -  еда");
+                    Enemy boss = GetRandomBoss();
+                    StartBattle(player, boss);
+                }
+                else
+                {
+                    // 50/50 шанс сундука или врага
+                    if (random.Next(2) == 0) // 0 - враг, 1 - сундук
+                    {
+                        Enemy enemy = GetRandomEnemy();
+                        StartBattle(player, enemy);
+                    }
+                    else
+                    {
+                        Chest chest = new Chest();
+                        chest.Open(player);
+                    }
+                }
+
+                if (player.HP <= 0)
+                {
+                    Console.WriteLine("\n=== ИГРА ОКОНЧЕНА ===");
+                    Console.WriteLine($"Вы продержались {turnCount} ходов!");
                     break;
                 }
-            case categories.groceries:
-                {
-                    Console.WriteLine("Категория -  бакалея");
-                    break;
-                }
-            case categories.careProducts:
-                {
-                    Console.WriteLine("Категория -  товары для ухода");
-                    break;
-                }
+
+                Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                Console.ReadKey();
+            }
         }
-
-
-
-    }
-}
